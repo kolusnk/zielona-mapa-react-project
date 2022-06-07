123866f33325927ffdbc4cca243aebc1d690e3c6
kolusnk <skorus.jakub@gmail.com>
2022-06-07 16:50:41 +0200

    instalacja mapy - leaflet, wstepne ustawienia i stylowanie

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
                         </Route>
                     </Switch>
                 </div>
+
                 <Footer/>
             </div>
         </Router>
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import {Template} from "../components/Template/Template";
-import {Banner} from "../components/Banner/Banner";
-import {HowItWorks} from "../components/HowItWorks/HowItWorks";
-import {GetToForm} from "../components/GetToForm/GeToForm";
+import './map.scss'
+import 'leaflet/dist/leaflet.css'
+
+import {MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet'
 
 export const Map = () => {
+    const center = [52.23043861355005, 21.010922836222367]
     return (
-        <>
-            <div  className="container"style={{padding: "5rem 0"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Accusamus asperiores doloribus enim eos fuga laborum veritatis voluptas?
-                Accusamus animi aperiam beatae blanditiis, culpa cumque cupiditate debitis
-                distinctio dolor dolorem doloremque ducimus enim, eum eveniet id in ipsum
-                iste iusto laudantium magnam minima molestias, nostrum officia omnis pariatur
-                possimus quae qui quibusdam quidem ratione rerum sequi sunt suscipit tempore
-                vel voluptate voluptatibus! Adipisci, hic illum ipsam itaque laboriosam maiores
-                numquam perferendis porro possimus ratione saepe sunt unde? Alias dolor error,
-                ex facilis illum iure minima modi molestiae natus nostrum numquam odio officia
-                porro qui quis, sed tempore temporibus, voluptate voluptatibus.</div>
-        </>
+        <div className='container map'>
+            <section className='map__container'>
+                <div id='map'>
+                    <MapContainer center={[52.23, 21.01]} zoom={10} scrollWheelZoom={true}
+                                  style={{width: "100%", height: "100%"}}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[52.23043861355005, 21.010922836222367]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            </section>
+        </div>
 
     )
 }
