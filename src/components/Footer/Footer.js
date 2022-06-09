fc451a7bc24f13dc5fa04a51cad8ee5f70b6b6e5
kolusnk <skorus.jakub@gmail.com>
2022-06-09 06:35:03 +0200

    stylowanie - footer + favicon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,13 +9,17 @@ export function Footer() {
                 <Link className="header__logo" to="/">ZielonaMapa.pl</Link>
                 <div className='footer__contact'>
                     <h4 className="footer__contact-title">Skontaktuj się z nami</h4>
-                    <div className="footer__contact-adres">Adres:
-                        <p>ZielonaMapa.pl</p>
-                        <p>ul. Podleśna 33</p>
-                        <p>03-303 Leśniczówka Wielka</p>
+                    <div className="contact">
+                        <div className="contact__adres">
+                            <p>ZielonaMapa.pl</p>
+                            <p>ul. Podleśna 33</p>
+                            <p>03-303 Leśniczówka Wielka</p>
+                        </div>
+                        <div className="contact__details">
+                            <p className='contact__details-email'>e-mail: zielona@mapa.pl</p>
+                            <p className='contact__details-tel'>telefon: 721 446 137</p>
+                        </div>
                     </div>
-                    <p className='footer__contact-email'>e-mail: zielona@mapa.pl</p>
-                    <p className='footer__contact-tel'>telefon: 737 373 737</p>
                 </div>
             </div>
         </footer>
