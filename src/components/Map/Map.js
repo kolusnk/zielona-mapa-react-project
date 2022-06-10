ac754a2c584bc394136e8d1ca50be07485110bd1
kolusnk <skorus.jakub@gmail.com>
2022-06-10 11:57:59 +0200

    pobieranie danych z firebase i wyswietlanie na mapie

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {MapContainer, Popup, TileLayer} from "react-leaflet";
 import {PinMarker} from "./PinMarker";
 import './map.scss'
 import 'leaflet/dist/leaflet.css'
+import db from '../../firebase/firebase';
+import {onSnapshot, collection} from 'firebase/firestore'
+
 
 export function Map() {
-    const center = [52.23043861355005, 21.010922836222367]
+    const [markers, setMarkers] = useState([]);
+    console.log(markers)
+    useEffect(() => {
+        onSnapshot(collection(db, "pinmarkers"), (snapshot) => {
+            setMarkers(snapshot.docs.map(doc => ({...doc.data(), id: doc.id})))
+        });
+    }, [])
+
+    const center = [52.33043861355005, 21.010922836222367]
     return (
         <section className='container map__container'>
             <div id='map'>
-                <MapContainer center={[52.23, 21.01]} zoom={7} scrollWheelZoom={true}
+                <MapContainer center={center} zoom={7} scrollWheelZoom={true}
                               style={{width: "100%", height: "100%"}}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <PinMarker position={[52.23043861355005, 21.010922836222367]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br/> Easily customizable.
-                        </Popup>
-                    </PinMarker>
+
+                    {
+                        markers.map(marker => {
+                            return <PinMarker key={marker.id} position={[marker.positionX, marker.positionY]}>
+                                <Popup>
+                                    {marker.positionX} {marker.positionY}
+                                </Popup>
+                            </PinMarker>
+                        })
+                    }
                 </MapContainer>
             </div>
         </section>
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../components/Map/map.scss'
 import 'leaflet/dist/leaflet.css'
-
 import {MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet'
 import {PinMarker} from "../components/Map/PinMarker";
 import {Template} from "../components/Template/Template";
