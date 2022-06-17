e8fa23bff5964f93ebde952b7eaa19d74cbf0b89
kolusnk <skorus.jakub@gmail.com>
2022-06-17 09:14:20 +0200

    poprawa responsywnosci

--- src/components/Form/FormText.js
+++ src/components/Form/FormText.js
@@ -5,7 +5,7 @@ export function FormText() {
     return (
         <div className="form-text__container">
             <p className='form-text__subtitle'>Pomóż nam uzupełnić naszą mapę</p>
-            <h2 className="form-text__title">Co chcesz nam powiedzieć o zieleni w okół Ciebie?</h2>
+            <h2 className="form-text__title">Co chcesz nam powiedzieć o zieleni wokół Ciebie?</h2>
         </div>
     )
 }
