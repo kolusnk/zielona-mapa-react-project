6473b1f0cee4a320bafa83e1f46c9592fb7ae45e
kolusnk <skorus.jakub@gmail.com>
2022-06-20 21:29:17 +0200

    build, and readme 3

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,167 +1,150 @@
-import React, {useState} from 'react'
-import {Input} from "./Input";
-import {Select} from "./Select";
-import {GoogleMapSearch} from "./GoogleMapSearch";
-import {validate} from "./validate";
-import db from '../../firebase/firebase';
-import {addDoc, collection} from 'firebase/firestore'
-import {geocodeByAddress, getLatLng} from "react-places-autocomplete";
-
+import React, { useState } from "react";
+import { Input } from "./Input";
+import { Select } from "./Select";
+import { GoogleMapSearch } from "./GoogleMapSearch";
+import { validate } from "./validate";
+import db from "../../firebase/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 
 export function Form() {
+  // określenie stanów i utworzenie przydatnych zmiennych //
 
-    // określenie stanów i utworzenie przydatnych zmiennych //
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    trees: "",
+    view: "",
+    noise: "",
+    floor: "",
+  });
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null,
+  });
+  const [adress, setAdress] = useState("");
+  const [errorMessages, setErrorMessages] = useState(null);
+  const [adressError, setAdressError] = useState(null);
+  const [successInfo, setSuccessInfo] = useState(null);
+  const pinmarkersCollection = collection(db, "pinmarkers");
 
-    const [values, setValues] = useState({
-        name: '',
-        email: '',
-        trees: '',
-        view: '',
-        noise: '',
-        floor: ''
+  // obsługa zmiany wartości inputów i uaktualnienie ich stanu//
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    console.log([event.target]);
+    setValues((prevValues) => {
+      return {
+        ...prevValues,
+        [name]: value,
+      };
     });
-    const [coordinates, setCoordinates] = useState({
-        lat: null,
-        lng: null
-    });
-    const [adress, setAdress] = useState("");
-    const [errorMessages, setErrorMessages] = useState(null)
-    const [adressError, setAdressError] = useState(null)
-    const [successInfo, setSuccessInfo] = useState(null)
-    const pinmarkersCollection = collection(db, 'pinmarkers')
+  }
 
-    // obsługa zmiany wartości inputów i uaktualnienie ich stanu//
+  // obsługa WYBORU ADRESU i uaktualnienie jego stanu //
 
-    function handleChange(event) {
-        const {name, value} = event.target
-        console.log([event.target])
-        setValues(prevValues => {
-            return {
-                ...prevValues,
-                [name]: value,
-            }
-        })
+  const handleSelectLocation = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAdress(value);
+    setCoordinates(latLng);
+  };
+
+  // wysyłanie wartości formularza do bazy danych //
+
+  const sendToDatabase = async (event) => {
+    event.preventDefault();
+    const errorMessages = validate(values);
+    setErrorMessages(errorMessages);
+    const successMessage = "wysłano, dziękujemy!";
+    const adressErrorMessage = "nie podano adresu";
+
+    // jeśli są błędy - nie wysyłaj //
+
+    if (errorMessages) return;
+    if (!adress) {
+      setAdressError(adressErrorMessage);
+      return;
     }
 
-    // obsługa WYBORU ADRESU i uaktualnienie jego stanu //
+    // jeśli nie ma błędów - wysyłaj i resetuj stan inputów //
 
-    const handleSelectLocation = async value => {
-        const results = await geocodeByAddress(value);
-        const latLng = await getLatLng(results[0]);
-        setAdress(value);
-        setCoordinates(latLng);
-    };
+    await addDoc(pinmarkersCollection, {
+      name: values.name,
+      email: values.email,
+      adress: adress,
+      positionX: coordinates.lat + Math.random() * 0.0005,
+      positionY: coordinates.lng + Math.random() * 0.0005,
+      trees: values.trees,
+      view: values.view,
+      noise: values.noise,
+      floor: values.floor,
+    });
+    setValues({ name: "", email: "", trees: "", view: "", noise: "", floor: "" });
+    setAdress("");
+    setAdressError("");
+    setSuccessInfo(successMessage);
+  };
 
-    // wysyłanie wartości formularza do bazy danych //
+  return (
+    <form className="form">
+      <Input label="Imię:" type="text" name="name" value={values.name} errorMessage={errorMessages?.name} onChange={handleChange} />
+      <Input label="Email:" type="email" name="email" value={values.email} errorMessage={errorMessages?.email} onChange={handleChange} />
 
-    const sendToDatabase = async (event) => {
-        event.preventDefault()
-        const errorMessages = validate(values);
-        setErrorMessages(errorMessages)
-        const successMessage = 'wysłano, dziękujemy!'
-        const adressErrorMessage = 'nie podano adresu'
+      <GoogleMapSearch
+        label="Adres:"
+        value={adress}
+        onChange={setAdress}
+        onSelect={handleSelectLocation}
+        adressError={adressError}
+        errorMessage={errorMessages?.adress}
+      />
 
-        // jeśli są błędy - nie wysyłaj //
+      <Select
+        label="Ilość drzew które widzisz z okna:"
+        name="trees"
+        value={values.trees}
+        options={["0", "1-5", "5-10", "10-20", "więcej...", "park", "las"]}
+        errorMessage={errorMessages?.trees}
+        onChange={handleChange}
+      />
 
-        if (errorMessages) return
-        if (!adress) {
-            setAdressError(adressErrorMessage)
-            return
-        }
+      <Select
+        label="Jaki jest widok z okna:"
+        name="view"
+        value={values.view}
+        errorMessage={errorMessages?.view}
+        options={["podwórko", "parking", "ulica", "otwarta przestrzeń", "natura", "inne"]}
+        onChange={handleChange}
+      />
 
-        // jeśli nie ma błędów - wysyłaj i resetuj stan inputów //
+      <Input
+        label="Piętro na którym mieszkasz:"
+        type="number"
+        name="floor"
+        value={values.floor}
+        min={0}
+        max={11}
+        errorMessage={errorMessages?.floor}
+        onChange={handleChange}
+      />
 
-        await addDoc(pinmarkersCollection, {
-            name: values.name,
-            email: values.email,
-            adress: adress,
-            positionX: coordinates.lat + (Math.random() * 0.0005),
-            positionY: coordinates.lng + (Math.random() * 0.0005),
-            trees: values.trees,
-            view: values.view,
-            noise: values.noise,
-            floor: values.floor
-        })
-        setValues({name: '', email: '', trees: '', view: '', noise: '', floor: ''})
-        setAdress('')
-        setAdressError('')
-        setSuccessInfo(successMessage)
-    }
+      <Select
+        label="Poziom hałasu:"
+        name="noise"
+        value={values.noise}
+        errorMessage={errorMessages?.noise}
+        options={["cicho", "głośno"]}
+        onChange={handleChange}
+      />
 
-
-    return (
-        <form className='form'>
-            <Input
-                label='Imię:'
-                type='text'
-                name='name'
-                value={values.name}
-                errorMessage={errorMessages?.name}
-                onChange={handleChange}
-            />
-            <Input
-                label='Email:'
-                type='email'
-                name='email'
-                value={values.email}
-                errorMessage={errorMessages?.email}
-                onChange={handleChange}
-            />
-
-            <GoogleMapSearch
-                label='Adres:'
-                value={adress}
-                onChange={setAdress}
-                onSelect={handleSelectLocation}
-                adressError={adressError}
-                errorMessage={errorMessages?.adress}
-
-            />
-
-            <Select
-                label='Ilość drzew które widzisz z okna:'
-                name='trees'
-                value={values.trees}
-                options={['0', '1-5', '5-10', '10-20', 'więcej...', 'park', 'las']}
-                errorMessage={errorMessages?.trees}
-                onChange={handleChange}
-            />
-
-            <Select
-                label='Jaki jest widok z okna:'
-                name='view'
-                value={values.view}
-                errorMessage={errorMessages?.view}
-                options={['podwórko', 'parking', 'ulica', 'otwarta przestrzeń', 'natura', 'inne']}
-                onChange={handleChange}
-            />
-
-
-            <Input
-                label='Piętro na którym mieszkasz:'
-                type='number'
-                name='floor'
-                value={values.floor}
-                min={0}
-                max={11}
-                errorMessage={errorMessages?.floor}
-                onChange={handleChange}
-            />
-
-            <Select
-                label='Poziom hałasu:'
-                name='noise'
-                value={values.noise}
-                errorMessage={errorMessages?.noise}
-                options={['cicho', 'głośno']}
-                onChange={handleChange}
-            />
-
-
-            <div className='button-and-succes'>
-                <button className='btn btn-primary btn-green' onClick={sendToDatabase}>Wyślij</button>
-                {successInfo && <span className='succes-message'>({successInfo})</span>}<br/>
-            </div>
-        </form>
-    )
+      <div className="button-and-succes">
+        <button className="btn btn-primary btn-green" onClick={sendToDatabase}>
+          Wyślij
+        </button>
+        {successInfo && <span className="succes-message">({successInfo})</span>}
+        <br />
+      </div>
+    </form>
+  );
 }
--- src/components/Map/MapComponent.js
+++ src/components/Map/MapComponent.js
@@ -1,37 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import {SearchInput} from "./SearchInput"
-import './map.scss'
-import {Map, TileLayer} from "react-leaflet";
-import 'leaflet/dist/leaflet.css'
-import db from '../../firebase/firebase';
-import {onSnapshot, collection} from 'firebase/firestore'
-import {AllMarkers} from './AllMarkers';
-
+import React, { useState, useEffect } from "react";
+import { SearchInput } from "./SearchInput";
+import "./map.scss";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import db from "../../firebase/firebase";
+import { onSnapshot, collection } from "firebase/firestore";
+import { AllMarkers } from "./AllMarkers";
 
 export function MapComponent() {
-    const [markers, setMarkers] = useState([]);
-    useEffect(() => {
-        onSnapshot(collection(db, "pinmarkers"), (snapshot) => {
-            setMarkers(snapshot.docs.map(doc => ({...doc.data(), id: doc.id})))
-        });
-    }, [])
+  const [markers, setMarkers] = useState([]);
+  useEffect(() => {
+    onSnapshot(collection(db, "pinmarkers"), (snapshot) => {
+      setMarkers(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+  }, []);
 
-
-    const center = [52.096696599828014, 19.41233664228455]
-    return (
-        <section className='container map__container'>
-            <div className='map'>
-                <Map center={center} zoom={6} scrollWheelZoom={true}
-                     style={{width: "100%", height: "100%"}}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <SearchInput/>
-                    <AllMarkers markers={markers}/>
-
-                </Map>
-            </div>
-        </section>
-    )
+  const center = [52.096696599828014, 19.41233664228455];
+  return (
+    <section className="container map__container">
+      <div className="map">
+        <Map center={center} zoom={6} scrollWheelZoom={true} style={{ width: "100%", height: "100%" }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <SearchInput />
+          <AllMarkers markers={markers} />
+        </Map>
+      </div>
+    </section>
+  );
 }
